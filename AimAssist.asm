ab428c6c9c61eb171ea62d855eb2379f88c97eb4
morohoro <154812080+morohoro@users.noreply.github.com>
2025-07-05 16:04:32 +0200

    Update AimAssist.asm

--- AimAssist.asm
+++ AimAssist.asm
@@ -37,7 +37,7 @@
 @l_Start:
     pop eax                     ; Load 0x7FFE02E0 (GetAsyncKeyState)
     mov eax, DWORD PTR [eax]    ; Get hotkey state
-    mov VR9, eax                ; Store in VR9 (boneIndex)
+    mov VR7, eax                ; Store in VR7 (boneIndex)
     pop eax                     ; Load ClientPlayerManager (0x0238EB58)
     mov eax, DWORD PTR [eax]    ; Dereference
     mov ecx, VR0                ; VR0 = GLOBAL_MinimumAddress (0x10000)
@@ -47,7 +47,8 @@
     mov VR1, eax                ; VR1 = ClientPlayerManager
     mov eax, VR1
     add eax, 0x4                ; ClientPlayerManager + 0x13C (LocalPlayer)
-    ; ... (repeat add eax, 0x4 49 times to reach 0x13C = 316 decimal)
+                                
+                                ; ... (repeat add eax, 0x4 49 times to reach 0x13C = 316 decimal)
     mov eax, DWORD PTR [eax]    ; Get LocalPlayer
     mov ecx, VR0
     sub eax, ecx
@@ -56,7 +57,7 @@
     mov VR2, eax                ; VR2 = LocalPlayer
     mov eax, VR2
     add eax, 0x4                ; LocalPlayer + 0x1C34 (myTeam)
-    ; ... (repeat add eax, 0x4 1805 times to reach 0x1C34 = 7220 decimal)
+                                ; ... (repeat add eax, 0x4 1805 times to reach 0x1C34 = 7220 decimal)
     mov eax, DWORD PTR [eax]
     mov ecx, VR0
     sub eax, ecx
@@ -65,7 +66,7 @@
     mov VR3, eax                ; VR3 = myTeam
     mov eax, VR2
     add eax, 0x4                ; LocalPlayer + 0x3A8 (soldier)
-    ; ... (repeat add eax, 0x4 234 times to reach 0x3A8 = 936 decimal)
+                                ; ... (repeat add eax, 0x4 234 times to reach 0x3A8 = 936 decimal)
     mov eax, DWORD PTR [eax]
     mov ecx, VR0
     sub eax, ecx
@@ -74,7 +75,7 @@
     mov VR4, eax                ; VR4 = soldier
     mov eax, VR4
     add eax, 0x4                ; soldier + 0x490 (boneComponent)
-    ; ... (repeat add eax, 0x4 292 times to reach 0x490 = 1168 decimal)
+                                ; ... (repeat add eax, 0x4 292 times to reach 0x490 = 1168 decimal)
     mov eax, DWORD PTR [eax]
     mov ecx, VR0
     sub eax, ecx
@@ -83,7 +84,7 @@
     mov VR5, eax                ; VR5 = boneComponent
     mov eax, VR5
     add eax, 0x4                ; boneComponent + 0x150 (boneArray)
-    ; ... (repeat add eax, 0x4 84 times to reach 0x150 = 336 decimal)
+                                ; ... (repeat add eax, 0x4 84 times to reach 0x150 = 336 decimal)
     mov eax, DWORD PTR [eax]
     xchg eax, ebx               ; ebx = boneArray
     pop eax                     ; Load precomputed boneIndex * 0x50
@@ -92,17 +93,17 @@
     mov VR6, eax                ; VR6 = myBonePtr
     mov eax, VR6
     add eax, 0x4                ; myBonePtr + 0x30 (myX)
-    ; ... (repeat add eax, 0x4 12 times to reach 0x30 = 48 decimal)
+                                ; ... (repeat add eax, 0x4 12 times to reach 0x30 = 48 decimal)
     mov eax, DWORD PTR [eax]
     mov VR2, eax                ; VR2 = myX
     mov eax, VR6
     add eax, 0x4                ; myBonePtr + 0x34 (myY)
-    ; ... (repeat add eax, 0x4 13 times to reach 0x34 = 52 decimal)
+                                ; ... (repeat add eax, 0x4 13 times to reach 0x34 = 52 decimal)
     mov eax, DWORD PTR [eax]
     mov VR3, eax                ; VR3 = myY
     mov eax, VR6
     add eax, 0x4                ; myBonePtr + 0x38 (myZ)
-    ; ... (repeat add eax, 0x4 14 times to reach 0x38 = 56 decimal)
+                                ; ... (repeat add eax, 0x4 14 times to reach 0x38 = 56 decimal)
     mov eax, DWORD PTR [eax]
     mov VR4, eax                ; VR4 = myZ
     pop eax                     ; Load ClientPlayerManager + 0x344 (playerList)
@@ -116,7 +117,7 @@
     mov VR8, eax                ; VR8 = PlayerListPos
     mov eax, VR7
     add eax, 0x4                ; playerList + 0x100 (64 * 4 = 256)
-    ; ... (repeat add eax, 0x4 64 times to reach 0x100 = 256 decimal)
+                                ; ... (repeat add eax, 0x4 64 times to reach 0x100 = 256 decimal)
     mov VR9, eax                ; VR9 = PlayerListEnd
 
 @Loop:
@@ -138,7 +139,7 @@
     xchg eax, ebx
     mov eax, ebx                ; ebx = player
     add eax, 0x4                ; player + 0x1C34 (team)
-    ; ... (repeat add eax, 0x4 1805 times to reach 0x1C34 = 7220 decimal)
+                                ; ... (repeat add eax, 0x4 1805 times to reach 0x1C34 = 7220 decimal)
     mov eax, DWORD PTR [eax]
     xchg eax, ebx
     mov ecx, VR3                ; VR3 = myTeam
@@ -151,7 +152,7 @@
     xchg eax, ebx
     mov eax, ebx                ; ebx = player
     add eax, 0x4                ; player + 0x3A8 (soldier)
-    ; ... (repeat add eax, 0x4 234 times to reach 0x3A8 = 936 decimal)
+                                ; ... (repeat add eax, 0x4 234 times to reach 0x3A8 = 936 decimal)
     mov eax, DWORD PTR [eax]
     xchg eax, ebx
     mov ecx, VR0
@@ -164,7 +165,7 @@
     xchg eax, ebx
     mov eax, ebx                ; ebx = soldier
     add eax, 0x4                ; soldier + 0x548 (healthComponent)
-    ; ... (repeat add eax, 0x4 337 times to reach 0x548 = 1348 decimal)
+                                ; ... (repeat add eax, 0x4 337 times to reach 0x548 = 1348 decimal)
     mov eax, DWORD PTR [eax]
     xchg eax, ebx
     mov ecx, VR0
@@ -176,7 +177,7 @@
     xchg eax, edx
     xchg eax, ebx
     add eax, 0x4                ; healthComponent + 0x30 (health)
-    ; ... (repeat add eax, 0x4 12 times to reach 0x30 = 48 decimal)
+                                ; ... (repeat add eax, 0x4 12 times to reach 0x30 = 48 decimal)
     mov eax, DWORD PTR [eax]
     xchg eax, edx
     pop ecx                     ; Load 0x0 for health check
@@ -187,7 +188,7 @@
     xchg eax, ebx
     mov eax, ebx                ; ebx = soldier
     add eax, 0x4                ; soldier + 0x490 (boneComponent)
-    ; ... (repeat add eax, 0x4 292 times to reach 0x490 = 1168 decimal)
+                                ; ... (repeat add eax, 0x4 292 times to reach 0x490 = 1168 decimal)
     mov eax, DWORD PTR [eax]
     xchg eax, ebx
     mov ecx, VR0
@@ -199,7 +200,7 @@
     xchg eax, edx
     xchg eax, ebx
     add eax, 0x4                ; boneComponent + 0x150 (boneArray)
-    ; ... (repeat add eax, 0x4 84 times to reach 0x150 = 336 decimal)
+                                ; ... (repeat add eax, 0x4 84 times to reach 0x150 = 336 decimal)
     mov eax, DWORD PTR [eax]
     xchg eax, ebx               ; ebx = boneArray
     pop eax                     ; Load precomputed boneIndex * 0x50
@@ -208,22 +209,22 @@
     mov edx, eax                ; edx = enemyBonePtr
     xchg eax, edx
     add eax, 0x4                ; enemyBonePtr + 0x30 (enemyX)
-    ; ... (repeat add eax, 0x4 12 times to reach 0x30 = 48 decimal)
+                                ; ... (repeat add eax, 0x4 12 times to reach 0x30 = 48 decimal)
     mov eax, DWORD PTR [eax]
     xchg eax, edx
     mov VR1, eax                ; VR1 = enemyX
     mov eax, edx
     add eax, 0x4                ; enemyBonePtr + 0x34 (enemyY)
-    ; ... (repeat add eax, 0x4 13 times to reach 0x34 = 52 decimal)
+                                ; ... (repeat add eax, 0x4 13 times to reach 0x34 = 52 decimal)
     mov eax, DWORD PTR [eax]
     xchg eax, edx
     mov VR5, eax                ; VR5 = enemyY
     mov eax, edx
     add eax, 0x4                ; enemyBonePtr + 0x38 (enemyZ)
-    ; ... (repeat add eax, 0x4 14 times to reach 0x38 = 56 decimal)
+                                ; ... (repeat add eax, 0x4 14 times to reach 0x38 = 56 decimal)
     mov eax, DWORD PTR [eax]
     xchg eax, edx
-    mov VR6, eax                ; VR6 = enemy事先
+    mov VR6, eax                ; VR6 = enemyZ
 
 @NextPlayer:
     mov eax, VR8
